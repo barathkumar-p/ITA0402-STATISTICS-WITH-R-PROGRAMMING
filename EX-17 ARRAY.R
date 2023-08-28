3cb6c3bdcfeec6207e260e0396b37059e01bb1a8
barathkumar-p <81065407+barathkumar-p@users.noreply.github.com>
2023-08-28 09:59:17 +0530

    Add files via upload

---
+++ EX-17 ARRAY.R
@@ -0,0 +1,4 @@
+a=array(6:20,dim=c(3,3,3),dimnames=list(c("row1","row2","row3"),
+						                c("col1","col2","col3"),
+                                        c("part1","part2","part3")))
+print(a)
